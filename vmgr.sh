c5cb43e0671570c4b51caeca50bd6a9b9a61b008
Steve Mansour <sman@stevemansour.com>
2015-08-04 12:35:31 -0700

    added build version manager for the Jenkins environment

---
+++ vmgr.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+# Manage the version number for releases that will be archived to artifactory.
+# It assumes that the BUILD_NUMBER environment variable contains the build
+# number.  This environment variable is managed by Jenkins.  It also assumes
+# that we're working with gradle and its artifactory plugin.  The line that
+# controls the numbering begins with "currentVersion"
+
+# $ver will look like this:
+#	currentVersion=1.0.000000-SNAPSHOT
+#                      | |    |      ^--- can be either SNAPSHOT or RELEASE
+#                      | |    +---------- build number space
+#                      | +--------------- Minor version number
+#                      +----------------- Major version number
+ver=$( grep currentVersion gradle.properties )
+ver=${ver/currentVersion=/}
+verlen=${#ver}
+
+
+major=${ver/%.*/}
+majorlen=${#major}
+type=${ver/#*-/}
+typelen=${#type}
+len=$((verlen-typelen-1))
+subver1=${ver:0:$len}
+idx=$((1+majorlen))
+subver2=${subver1:$idx}
+minor=${subver2/%.*/}
+buildno=${subver2/#*./}
+
+if [  -z "${BUILD_NUMBER}" ]; then
+    echo "BUILD_NUMBER environment variable is not set"
+    exit 1
+fi
+
+buildno=$((BUILD_NUMBER))
+printf "currentVersion=%d.%d.%06d-$type\n" $major $minor $buildno > tmpfile
+sed '/currentVersion/d' gradle.properties >> tmpfile
+mv gradle.properties gradle.properties.bak
+mv tmpfile gradle.properties
